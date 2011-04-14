dff53475e77b95cab065964bcbe5cd71fef84a7e
unknown <montecalvos@.wit.private>
2011-04-14 00:42:18 -0400

    Updated DB files

---
+++ floreantpos/src/pos/POSApp.java
@@ -0,0 +1,44 @@
+/*
+ * POSApp.java
+ */
+
+package pos;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class POSApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new POSView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of POSApp
+     */
+    public static POSApp getApplication() {
+        return Application.getInstance(POSApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(POSApp.class, args);
+    }
+}
