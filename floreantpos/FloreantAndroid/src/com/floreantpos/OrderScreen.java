8195a8be4435c4167199b1eb2f0f980d95f923c3
Derek Lemery <dlemery@derekmacbook.local>
2011-04-28 11:42:07 -0400

    Implemented Database connection into actual application. Currently pulls one menu item.

--- floreantpos/FloreantAndroid/src/com/floreantpos/OrderScreen.java
+++ floreantpos/FloreantAndroid/src/com/floreantpos/OrderScreen.java
@@ -1,7 +1,10 @@
 package com.floreantpos;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 //import android.content.Intent;
 //import android.widget.*;
@@ -15,5 +18,18 @@ protected void onCreate(Bundle savedInstanceState) {
 	//com.floreantpos.TestDatabase.main(null);
 	//TextView myTextView2 = (TextView) findViewById(R.id.FooPrint);
 	//myTextView2.setText("moooooooo");
+	
+	
+	Button BeveragesButton = (Button)findViewById(R.id.Beverages);
+	
+	BeveragesButton.setOnClickListener(new View.OnClickListener() {
+
+        @Override
+        public void onClick(View view) {
+          Intent intent = new Intent(OrderScreen.this, Beverages.class);
+          startActivity(intent);
+        }
+
+      });
 }
 }
